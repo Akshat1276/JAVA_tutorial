10ea9a5de1eace41e648ac9f7a0e0233dd3d7d82
Akshat Yadav <71210501+Akshat1276@users.noreply.github.com>
2024-06-03 17:42:18 +0530

    Create switch.java

---
+++ switch.java
@@ -0,0 +1,15 @@
+class example {
+    public static void main(String[] args) {
+        int LastDig = 7;
+        switch (LastDig){
+            case 3:
+            System.out.println("three");
+            break;
+            case 6:
+            System.out.println("Six");
+            break;
+            default:
+            System.out.println("Not three or six");
+        }
+    }
+}
