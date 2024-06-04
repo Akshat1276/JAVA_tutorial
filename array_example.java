8a8d8a547cfcaf2cfb5dd77a654f9d5cdf66f08d
Akshat Yadav <71210501+Akshat1276@users.noreply.github.com>
2024-06-04 13:56:18 +0530

    Create array_example.java

---
+++ array_example.java
@@ -0,0 +1,10 @@
+class example {
+    public static void main(String[] args) {
+        int[] nun = {1, 2, 3, 4, 5};
+        int ilu = 0;
+        while (ilu < nun.length) {
+            System.out.println(nun[ilu]);
+            ilu++;            
+        }
+    }
+}
