7998e08943a79ea12f7ccb19d646b1035a248fe3
Akshat Yadav <71210501+Akshat1276@users.noreply.github.com>
2024-06-07 16:43:44 +0530

    Create Multiple_Attributes.java

---
+++ Multiple_Attributes.java
@@ -0,0 +1,10 @@
+public class Main {
+    int x = 10;
+  
+    public static void main(String[] args) {
+        Main myBob = new Main();
+        Main myBOB2 = new Main();
+        System.out.println(myBob.x);
+        System.out.println(myBOB2.x);
+    }
+  }
