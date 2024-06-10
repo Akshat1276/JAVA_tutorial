d44e0cf8f120a2cc489aa7c190db754d458ae044
Akshat Yadav <71210501+Akshat1276@users.noreply.github.com>
2024-06-10 23:16:26 +0530

    Update Editing_Attributes.java

--- Editing_Attributes.java
+++ Editing_Attributes.java
@@ -3,7 +3,7 @@ public class Main {
   
     public static void main(String[] args) {
       Main myobj = new Main();
-      myObj.x = 25; // x is now 25
+      myObj.x = 25; // x is now 2
       System.out.println(myobj.x);
     }
   }
