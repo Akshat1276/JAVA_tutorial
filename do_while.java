a28595d2dda155d993ccc6fcbfdb4393a9241d25
Akshat Yadav <71210501+Akshat1276@users.noreply.github.com>
2025-02-01 23:15:22 +0530

    Update do_while.java

--- do_while.java
+++ do_while.java
@@ -2,7 +2,7 @@ class example {
     public static void main(String[] args) {
         int i=1;
         do {
-            system.out.println(i);
+            System.out.println(i);
         }
         while (i<10);
 }
